3dbb612a841befa44281c3be5060a7eeddfce6e0
geonhwi <ghghghgh0802@naver.com>
2026-03-20 15:49:18 +0900

    feat: 회원가입 API 구현 및 관리자 페이지 등록
    이메일을 username으로 사용하는 유저 생성 로직 구현
    회원가입용 Serializer 및 APIView 작성 완료
    /api/accounts/signup/ 경로 연결 및 가입 테스트 성공
    관리자(Admin) 페이지에 User 모델 등록

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 
-# Create your views here.
+class SignupView(APIView):
+    # 프론트엔드에서 데이터를 '보낼 때(POST)' 실행되는 함수입니다.
+    def post(self, request):
+        # 1. 프론트엔드가 보낸 데이터를 번역기(Serializer)에 넣습니다.
+        serializer = UserSerializer(data=request.data)
+
+        # 2. 데이터가 형식에 맞게 잘 들어왔는지 검사합니다.
+        if serializer.is_valid():
+            # 3. 통과하면 DB에 저장(가입)합니다.
+            serializer.save()
+            return Response({"message": "회원가입이 성공적으로 완료되었습니다!"}, status=status.HTTP_201_CREATED)
+
+        # 4. 데이터에 문제가 있으면 에러 이유를 프론트엔드에 알려줍니다.
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,9 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include를 추가로 불러옵니다.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # 프론트엔드가 http://127.0.0.1:8000/api/accounts/... 로 시작하는 주소로 접속하면, 
+    # accounts 폴더 안의 urls.py로 길을 안내해 줍니다.
+    path('api/accounts/', include('accounts.urls')),
 ]
