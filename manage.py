bc185a975b4a1f5033e8a73048cd210bc32b3513
Myung-Il <ktr040415@gmail.com>
2026-04-03 21:16:32 +0900

    chore: requirement.txt를 이용한 자동 모듈 설치
    1. pip freeze > requirements.txt를 이용해서 모듈 설치
    
    2. manage.py에서 실행 하면 자동으로 모듈 설치
    
    새로운 모듈이 생기는 것이 아닌 이상 모듈은 자동으로 설치됨.
    모듈을 추가했을 경우
    1번 과정을 하면 된다.

--- manage.py
+++ manage.py
@@ -2,7 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import subprocess
 
+# 1. 필수 모듈 자동 설치 함수
+def install_requirements():
+    req_file = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    if os.path.exists(req_file):
+        print("필수 모듈을 확인하고 설치합니다. 잠시만 기다려주세요...")
+        try:
+            # pip install -r requirements.txt 명령어 실행
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", req_file])
+            print("모듈 설치 완료!\n")
+        except subprocess.CalledProcessError:
+            print("모듈 설치 중 오류가 발생했습니다.")
+            sys.exit(1)
+
+# 2. 다른 외부 모듈을 불러오기 전에 가장 먼저 실행!
+install_requirements()
 
 def main():
     """Run administrative tasks."""
